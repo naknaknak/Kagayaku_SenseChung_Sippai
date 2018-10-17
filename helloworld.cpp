07eab6e3df06a9587a2820dd67f4edc08122acbf
nak_nak_nak <knh192@gmail.com>
2018-10-17 20:41:46 +0900

    초기 자료들

---
+++ helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout<<"Hello World! via int"<<endl;
+  return 0;
+}
+
+void main(void)
+{
+  cout<<"Hello World! via void"<<endl;
+}
